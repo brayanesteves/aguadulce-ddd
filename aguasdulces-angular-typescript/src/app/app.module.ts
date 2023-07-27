3e385c03c4ee3daa05b942bcd9f090cd23426a8d
Brayan Esteves <brayan.esteves93@gmail.com>
2023-07-26 21:33:48 -0400

    Correct Fix [26.07.2023]

--- aguasdulces-angular-typescript/src/app/app.module.ts
+++ aguasdulces-angular-typescript/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { ContactManagerComponent } from './contact-manager/contact-manager.component';
-import { AddContactComponent } from './add-contact/add-contact.component';
-import { EditContactComponent } from './edit-contact/edit-contact.component';
-import { ViewContactComponent } from './view-contact/view-contact.component';
-import { SpinnerComponent } from './spinner/spinner.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ContactManagerComponent } from './components/contact-manager/contact-manager.component';
+import { AddContactComponent } from './components/add-contact/add-contact.component';
+import { EditContactComponent } from './components/edit-contact/edit-contact.component';
+import { ViewContactComponent } from './components/view-contact/view-contact.component';
+import { SpinnerComponent } from './components/spinner/spinner.component';
 
 @NgModule({
   declarations: [
